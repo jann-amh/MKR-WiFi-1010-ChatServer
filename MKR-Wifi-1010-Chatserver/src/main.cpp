746774f1603346e3ccd6a3b2594ddda00f5bf486
Jann Amhausend <58480669+jann-amh@users.noreply.github.com>
2021-06-11 22:23:05 +0200

    added main cpp file

--- MKR-Wifi-1010-Chatserver/src/main.cpp
+++ MKR-Wifi-1010-Chatserver/src/main.cpp
@@ -1,9 +1,86 @@
 #include <Arduino.h>
+#include <ESPAsyncWebServer.h>
+#include <ESP8266WiFi.h>
+#include <WebSocketsServer.h>
+#include <Hash.h>
+#include <ESP8266mDNS.h>
+
+const char* ssid = "ssid";
+const char* password = "password";
+
+AsyncWebServer server(80);
+WebSocketsServer webSocket = WebSocketsServer(81);
+
+#define USE_SERIAL Serial
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+
+    switch(type) {
+        case WStype_DISCONNECTED:
+            USE_SERIAL.printf("[%u] Disconnected!\n", num);
+            break;
+        case WStype_CONNECTED:
+            {
+                IPAddress ip = webSocket.remoteIP(num);
+                USE_SERIAL.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+            }
+            break;
+        case WStype_TEXT:
+
+            USE_SERIAL.printf("[%u] get Text: %s\n", num, payload);
+
+            // send data to all connected clients
+            webSocket.broadcastTXT(payload);
+            break;
+        
+    }
+
+}
 
 void setup() {
-  // put your setup code here, to run once:
+    // USE_SERIAL.begin(921600);
+    USE_SERIAL.begin(115200);
+
+    if(!SPIFFS.begin()){
+        USE_SERIAL.println("An Error has occurred while mounting SPIFFS");
+        return;
+    }
+
+    WiFi.softAP("chat", "password01");
+    
+    USE_SERIAL.println(WiFi.softAPIP());
+
+    /*WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    USE_SERIAL.println(WiFi.localIP());*/
+
+
+    // Route for root / web page
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send(SPIFFS, "/index.html", "text/html");
+    });
+
+    if(!MDNS.begin("chat")) {
+        USE_SERIAL.println("mDNS failed!");
+    } else {
+        USE_SERIAL.println("mDNS responder started!");
+    }
+    MDNS.addService("http", "tcp", 80);
+
+     // Start server
+    server.begin();
+
+    webSocket.begin();
+    webSocket.onEvent(webSocketEvent);
+
+    
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    MDNS.update();
+    webSocket.loop();
 }
