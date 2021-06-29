72f246f5ae59edd03c1bbc609d9e62db7a059a3a
Jann Amhausend <58480669+jann-amh@users.noreply.github.com>
2021-06-29 20:03:02 +0200

    new gitignore

--- MKR-Wifi-1010-Chatserver/data/style.js
+++ MKR-Wifi-1010-Chatserver/data/style.js
@@ -3,14 +3,14 @@ var input = document.getElementById("msg-input");
 var submit = document.getElementById("msg-submit");
 
 input.addEventListener("input", function () {
-	var content = input.value;
-	if(!input.value.replace(/\s/g, '').length) {
-		submit.classList.remove('filled'); // not working
-		console.log('no content');
-	} else {
-		submit.classList.add('filled');
-		console.log('content');
-	};
+  var content = input.value;
+  if (!input.value.replace(/\s/g, "").length) {
+    submit.classList.remove("filled"); // not working
+    console.log("no content");
+  } else {
+    submit.classList.add("filled");
+    console.log("content");
+  }
 });
 
 input.addEventListener("keyup", function (event) {
